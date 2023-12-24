a882e048004f63d89fd41e46033a5e9eb926cd8e
pencilCool <yhtangcoder@gmail.com>
2023-12-24 10:08:09 +0800

    update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -130,7 +130,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
+  // noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
